26563f7dec09620542df5ff9e72c6f2362460d65
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-03 23:48:56 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # パスワードは半角英数字混合であること
+  validates :password,format: { with: /\A[a-z0-9]+\z/i, message: "Include both letters and numbers"}
+  # メールアドレスが一意性であること
+  validates :email, uniqueness: { case_sensitive: false , message: "Already in use."}
+  with_options presence: true do
+    validates :nickname, :birthday
+      # ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力
+    validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+      # ユーザー本名のフリガナは全角（カタカナ）で入力
+    validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  end
 end
