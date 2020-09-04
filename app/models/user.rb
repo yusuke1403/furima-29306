b20f5f2cae11c80b81ef577a1b8f197932e6271a
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-04 19:22:13 +0900

    Rubocopの自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # パスワードは半角英数字混合であること
-  validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
   with_options presence: true do
     validates :nickname, :birthday
-      # ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力
-    validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-      # ユーザー本名のフリガナは全角（カタカナ）で入力
-    validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    # ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力
+    validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+    # ユーザー本名のフリガナは全角（カタカナ）で入力
+    validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
   end
 end
