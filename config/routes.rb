8270de38b5d648b5ce36842ed41573cd3a1085fd
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-07 17:55:53 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
   root to: "items#index"
   devise_for :users
+  resources :items, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
