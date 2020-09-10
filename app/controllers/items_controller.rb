46b6693698f9858350b85fa6ce0fc81676c4e870
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-10 13:37:31 +0900

    コントローラーのedit,updateの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,11 +19,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @items = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
-    @items = Item.update(params[:id])
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   def show
