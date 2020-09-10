4605c767379d2ddf1e87a5c6f545b1c633b36249
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-10 19:23:03 +0900

    ビューファイルに商品情報を反映

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,16 @@
 class PurchasesController < ApplicationController
   before_action :move_to_sign_up
+  before_action :set_item
 
   def index
-  end
-
-  def creates
+    @purhcase = Purchase.new
+    @item = Item.find(params[:item_id])
   end
 
   private
+
+  def set_item
+  end
 
   def move_to_sign_up
     redirect_to new_user_registration_path unless user_signed_in?
