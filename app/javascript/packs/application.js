f7bc646cb18cacee0034426c23b421038422afda
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-10 13:38:42 +0900

    editビューのpriceの自動計算用のJS追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../items/new")
+require("../items/edit")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
