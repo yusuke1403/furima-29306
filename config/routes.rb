bcfd99ecf87bb7bb83e94a1c94e3e1196ad8b55f
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-15 18:12:36 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
   resources :items do
-    resources :purchases, only: [:index]
+    resources :purchases, only: [:index, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
