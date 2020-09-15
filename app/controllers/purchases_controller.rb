30c743e5da18bd742e4372179091ae922e636cf3
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-15 18:23:11 +0900

    purchases_controllerの編集
    pay_itemにてカード情報をPAYJPに送信
    formオブジェクトを使用して複数テーブルを保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,16 +3,40 @@ class PurchasesController < ApplicationController
   before_action :set_item
 
   def index
-    @purhcase = Purchase.new
-    @item = Item.find(params[:item_id])
+    @purchase = ShippingAddressPurchase.new
+  end
+
+  def create
+    @purchase = ShippingAddressPurchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
   def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def move_to_sign_up
     redirect_to new_user_registration_path unless user_signed_in?
+  end
+
+  def purchase_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :tel_number, :item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency:'jpy'
+    )
   end
 end
