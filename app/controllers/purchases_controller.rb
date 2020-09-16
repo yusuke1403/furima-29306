549ad719a97e98c5f71eeedfd538da8f1af1e651
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-16 15:38:36 +0900

    未ログイン時の購入ボタン移行先をログインページへ

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -24,7 +24,7 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_sign_up
-    redirect_to new_user_registration_path unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def purchase_params
