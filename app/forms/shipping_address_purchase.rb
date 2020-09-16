21050ee5785f7a67a147441538f5c651cdfb70d8
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-09-16 15:22:49 +0900

    ShippingAddressPurchaseのバリデーションのエラーメッセージ追加

--- app/forms/shipping_address_purchase.rb
+++ app/forms/shipping_address_purchase.rb
@@ -6,9 +6,9 @@ class ShippingAddressPurchase
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, :address, :token
-    validates :tel_number, numericality: { only_integer: true }
+    validates :tel_number, numericality: { only_integer: true, message: 'Half-width number'}
   end
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "Select" }
 
   def save
     # 購入の情報を保存し、「purchase」という変数に入れている
